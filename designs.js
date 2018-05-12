ed6685d1c05b05c13fdff92ffac2f5ef6bdae96c
X <x@Xs-MacBook-Pro.local>
2018-05-12 14:29:04 -0400

    Define makeGrid Function

--- designs.js
+++ designs.js
@@ -1,12 +1,22 @@
 // Select color input
 // Select size input
-  let gridHeight = document.getElementById('inputHeight').value;
-  let gridWidth = document.getElementById('inputWidth').value;
-  let grid = document.getElementById('pixelCanvas'); 
+  
 // When size is submitted by the user, call makeGrid()
+
 
 function makeGrid() {
   // Your code goes here!
+  let gridHeight = document.getElementById('inputHeight').value;
+  let gridWidth = document.getElementById('inputWidth').value;
+  let grid = document.getElementById('pixelCanvas'); 
 
+  //Create a row append the columns and append row to grid
+  for(let row=1; row<=gridHeight; row++){
+  	let tr = document.createElement('tr');
+  	for(let col=1; col<=gridWidth; col++){
+  		tr.appendChild(document.createElement('td'));
+  	}
+  	grid.appendChild(tr)
+  }
 
 }
