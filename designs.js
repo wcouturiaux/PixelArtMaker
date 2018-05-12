b53a5e454a8a97949a2ca87db00aefe1705c535a
X <x@Xs-MacBook-Pro.local>
2018-05-12 18:46:24 -0400

    Add color grid functionality, move grid variable to be global

--- designs.js
+++ designs.js
@@ -1,14 +1,14 @@
 // Select color input
+
 // Select size input
   
 // When size is submitted by the user, call makeGrid()
-
+let grid = document.getElementById('pixelCanvas');
 
 function makeGrid() {
   // Your code goes here!
   let gridHeight = document.getElementById('inputHeight').value;
   let gridWidth = document.getElementById('inputWidth').value;
-  let grid = document.getElementById('pixelCanvas');
   grid.innerHTML ="";
 
   //Create a row append the columns and append row to grid
@@ -24,3 +24,11 @@ function makeGrid() {
 
 const submitBtn = document.getElementById('submit');
 submitBtn.addEventListener('click', function(evt){ evt.preventDefault(); makeGrid();});
+
+function colorGrid(evt){
+	let targetElement = evt.target;
+	let chosenColor= document.getElementById('colorPicker').value;
+	targetElement.style.backgroundColor = chosenColor;
+}
+
+grid.addEventListener('click', function(evt){evt.preventDefault(); colorGrid(evt);})
