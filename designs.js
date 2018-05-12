56a230d502ba2388a12abf19a22509134640412e
X <x@Xs-MacBook-Pro.local>
2018-05-12 13:16:22 -0400

    Initial Commit

---
+++ designs.js
@@ -0,0 +1,12 @@
+// Select color input
+// Select size input
+  let gridHeight = document.getElementById('inputHeight').value;
+  let gridWidth = document.getElementById('inputWidth').value;
+  let grid = document.getElementById('pixelCanvas'); 
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+  // Your code goes here!
+
+
+}
